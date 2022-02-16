d1fed7b4914460785637321000a13304e56ad43e
Mish <alpzona@mail.ru>
2022-02-16 20:47:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // входные данные
+        int price = 1690;
+        //логика
+        int miles = price / 20;
+        System.out.println("Количество миль: " + miles);
+    }
+}
